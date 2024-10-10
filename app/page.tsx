d2e3cecfa4120419b6e6551c335cc691f2329704
Leony C <costa.leony2@gmail.com>
2024-10-10 17:28:34 -0300

    Commit 2

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,21 @@ import PlaceList from "../components/PlaceList";
 
 export default function Home() {
   const [placeList, setPlaceList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const getPlaceList = async (value:string) => {
+    setIsLoading(true);
     setPlaceList([]);
     const res = await fetch("/api/places?textQuery=" + value);
     const data = await res.json();
-
     setPlaceList(data.places);
+    setIsLoading(false);
   }
 
   return (
     <div>
       <Hero getPlaceList={getPlaceList} />
-      {placeList? <PlaceList placeList={placeList} /> : null}
+      {placeList? <PlaceList isLoading={isLoading} placeList={placeList} /> : null}
     </div>
   );
 }
--- components/PlaceList.tsx
+++ components/PlaceList.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import PlaceCard from './PlaceCard'
 import SideDrawer from './SideDrawer'
 import Skelton from './Skelton';
 
-function PlaceList({ placeList }:any) {
+function PlaceList({ placeList, isLoading }:any) {
   const [selectedPlace, setSelectedPlace] = useState<any>(null);
 
   const closeDrawer = () => {
@@ -13,23 +13,25 @@ function PlaceList({ placeList }:any) {
   
   return (
     <div className='px-[10px] md:px-[120px] mt-10 relative z-10 mb-4'>
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-          {placeList.map((place:any, index:number) => (
-            <div key={index} className='hover:scale-105 transition-all' onClick={() => setSelectedPlace(place)}>
-              <PlaceCard place={place}/>
-            </div>
-          ))}
-        </div>
-        {placeList.length == 0 ? 
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-          {[1, 2, 3, 4, 5].map((item, index) => (
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {isLoading ? (
+          [1, 2, 3, 4, 5].map((item, index) => (
             <Skelton key={index} />
-          ))}
-        </div> : null}
-        {selectedPlace?
+          ))
+        ) : (
+          placeList.map((place: any, index: number) => (
+            <div key={index} className='hover:scale-105 transition-all' onClick={() => setSelectedPlace(place)}>
+              <PlaceCard place={place} />
+            </div>
+          ))
+        )}
+      </div>
+
+      {selectedPlace ? (
         <div className='fixed top-0 right-0'>
           <SideDrawer place={selectedPlace} closeDrawer={closeDrawer} />
-        </div> : null}
+        </div>
+      ) : null}
     </div>
   )
 }
